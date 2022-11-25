15f2b8de993ef5be4f5ecb77ed8807273b58cdcc
Samiux <infosecninjas@gmail.com>
2022-11-25 16:06:36 +0800

    Update echo_no_get_shell.c

--- echo_no_get_shell/echo_no_get_shell.c
+++ echo_no_get_shell/echo_no_get_shell.c
@@ -3,6 +3,8 @@ This challenge is one of the pwn challenges at HKCERT CTF 2022
 Organiser's writeup at https://hackmd.io/@blackb6a/hkcert-ctf-2022-i-en-3f8a9ef6
 Source code is copied from the video : https://www.youtube.com/watch?v=YODluCDBf5I&t=7937s
 
+Modified by Samiux on Nov 23, 2022
+
 Compile : gcc -o echo_no_get_shell echo_no_get_shell.c
 */
 
